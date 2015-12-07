a0c732948e73f0f01946f3bf5f94f0eea7a61bab
tangzhanran <344023305@qq.com>
2015-12-07 15:38:14 +0800

    up

--- public/javascripts/transmit.js
+++ public/javascripts/transmit.js
@@ -1,4 +1,4 @@
-function post(url, type, params){
+function get(url, params){
 
     $.ajax({
         url: 'http://127.0.0.1:3000/post',
@@ -16,4 +16,59 @@ function post(url, type, params){
         }
     })
 
+}
+
+function post(host , port, path, params){
+
+    $.ajax({
+        url: 'http://127.0.0.1:3000/post',
+        method: 'post',
+        dataType: 'json',
+        data: {host : host, port: port, path: path , type: 'post', params : params},
+        success: function (ret) {
+            console.log('post success, and result : ');
+            console.log(ret);
+            alert('post success' , ret );
+            add_result(ret);
+        },
+        error: function(err){
+            console.log('post fail , reason : %s', err);
+            alert('post fail ');
+        }
+    })
+
+}
+
+function add_result(object){
+    var str = '<div class="col-lg-6"> <p> time :' + new Date() +
+        ' result : </p>' +
+        '<br/>';
+
+    str += '<ul>';
+    for(var key in object) {
+        str += '<li> ' +
+            key + ' : ' + object[key] + '\n\t'+
+            '</li>'
+    }
+
+    str += '</ul>';
+    str += '</div>'
+    $('div#result').append(str);
+}
+
+function btn_transmit_click(){
+
+    //var url = $('#la_url').val();
+    //console.log('url :' , url);
+
+    var type= $("select[name=type]").find("option:selected").val();
+    console.log('type :' , type);
+    var params = $('#la_params').val();
+    console.log('params :' , params);
+
+    var host = $('#la_host').val();
+    var port = parseInt( $('#la_port').val() );
+    var path =  $('#la_path').val();
+
+    post(host , port, path, params);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/post', function(req, res, next){
+router.post('/post', function(request, response, next){
 
-  var body = req.body;
-  console.log('body : %j', req.body);
-  res.send(body);
+  var body = request.body;
+  console.log('body : %j', request.body);
+
+  if(body.type == 'post') {
+
+    var options = {
+      host : body.host,
+      port : body.port,
+      method: 'POST',
+      path: body.path,
+      handers:{
+        "Content-Type": 'application/json',
+        "Content-Length": body.params.length
+      }
+    }
+
+    var req = http.request(options, function(res){
+      if(res.statusCode == 200) {
+        var body = "";
+        res.on('data', function(data) {
+          body += data;
+        }).on('end', function(error){
+          response.send(200, body);
+        })
+      }
+
+      else {
+        response.send(200, {code: res.statusCode});
+      }
+
+    })
+
+    req.write(body.params);
+
+
+  } else {
+
+    http.get(body.url, function(res){
+      console.log("response : ", res.statusCode);
+    }).on('error', function(err){
+      console.log("err : ", e.message);
+    })
+
+  }
+
 
 });
+
+
 
 module.exports = router;
